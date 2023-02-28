595295da813167a9667c847336b4229f3e6ef54e
Mohamed Aziz Ben Ismail <aziz.270700@gmail.com>
2023-02-28 15:08:12 +0100

    correction d'une erreur dans le path

--- events/urls.py
+++ events/urls.py
@@ -4,7 +4,7 @@ from .views import *
 urlpatterns = [
     path('homepage/<int:id>',HomePage,name='HomePage'),
     path('eventStatic/',eventStatic,name='event_Static'),
-    path('hghhjb/',EventList,name='event_list'),
+    path('listevent/',EventList,name='event_list'),
     path('create_event/',create_event,name='create_event'),
     path('EventListClass/',EventListClass.as_view(),name='Event_ListClass'),
     path('EventDetailClass/<int:pk>',EventDetailClass.as_view(),name='Event_DetailClass'),
